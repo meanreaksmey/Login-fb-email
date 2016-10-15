8d7c29d673f874b4e54c678d63d78c85f1acffcc
reaksmey <meanreaksmey@gmail.com>
2016-10-16 01:28:33 +0700

    Upload new file

---
+++ Demo Set Layout/Demo Set Layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo Set Layout
+//
+//  Created by reaksmey on 10/15/16.
+//  Copyright © 2016 reaksmey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Demo Set Layout/Demo Set LayoutTests/Demo_Set_LayoutTests.swift
@@ -0,0 +1,36 @@
+//
+//  Demo_Set_LayoutTests.swift
+//  Demo Set LayoutTests
+//
+//  Created by reaksmey on 10/15/16.
+//  Copyright © 2016 reaksmey. All rights reserved.
+//
+
+import XCTest
+@testable import Demo_Set_Layout
+
+class Demo_Set_LayoutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
